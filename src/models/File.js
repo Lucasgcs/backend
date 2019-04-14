aa2f5377faafeaae02f591ae755363de912aad1e
Lucas Silva <lucasdevti@gmail.com>
2019-04-14 13:04:25 -0300

    Adiciona variaveis Ambiente

--- src/models/File.js
+++ src/models/File.js
@@ -20,7 +20,11 @@ const File = new mongoose.Schema(
 );
 
 File.virtual('url').get(function(){
-  return `http://localhost:3333/files/${encodeURIComponent(this.path)}`;
+  //definir variavel para ambiente de prod ou dev 
+
+  const url = process.env.URL || 'http://localhost:3333'
+  
+  return `${url}/files/${encodeURIComponent(this.path)}`;
 });
 
 module.exports = mongoose.model("File", File);
--- src/server.js
+++ src/server.js
@@ -57,4 +57,4 @@ app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
 app.use(require('./routes'));
 
 //define onde o servidor irá rodar
-server.listen(3333);
+server.listen(process.env.PORT || 3333);
